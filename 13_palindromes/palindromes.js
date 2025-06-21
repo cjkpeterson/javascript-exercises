aa8caca7cbd5ddfcb0cd2e6c9fd22393119e3996
Chase Peterson <cjkpeterson@gmail.com>
2025-06-21 12:02:49 -0700

    Complete 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let arr = word.split("");
+    arr = arr.filter(char => (char.charCodeAt(0) >= 48) && (char.charCodeAt(0) <= 122));
+    arr = arr.map(char => char.toLowerCase());
+    let reverse = [...arr].reverse();
+    return arr.every((letter, index) => letter === reverse[index]);
 };
 
 // Do not edit below this line
