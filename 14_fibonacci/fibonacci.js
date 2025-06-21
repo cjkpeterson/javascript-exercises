34c94913c766592cfa254f14be27b0d114fc8eb2
Chase Peterson <cjkpeterson@gmail.com>
2025-06-21 12:21:41 -0700

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (+a == 0) return 0;
+    if (+a < 0) return "OOPS";
+    let prev = 0;
+    let total = 1;
+    for (let i = 2; i <= +a; i++) {
+        [prev, total] = [total, prev + total];
+    }
+    return total;
 };
 
-// Do not edit below this line
+/* i p t
+   2 1 1
+   3 1 2
+   4 2 3
+
+*/
+
+// Do not edit below this line 
 module.exports = fibonacci;
