eed5f908b15e30cb81117bce06d2ab0a208bf0f4
Chase Peterson <cjkpeterson@gmail.com>
2025-06-21 12:49:21 -0700

    Complete 16 and all exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    people.sort((a, b) => {
+        let ageA = (('yearOfDeath' in a) ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+        let ageB = (('yearOfDeath' in b) ? b.yearOfDeath : currentYear) - b.yearOfBirth;
+        return ageB - ageA;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
